b7cbc36402c46f6cd10b0532aacb0bafcc38abc5
byf <124633367@qq.com>
2020-01-20 11:27:02 +0800

    测试jenkins部署war包

--- src/main/java/com/postmark/application/PostmarkApplication.java
+++ src/main/java/com/postmark/application/PostmarkApplication.java
@@ -2,9 +2,10 @@ package com.postmark.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PostmarkApplication {
+public class PostmarkApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
         SpringApplication.run(PostmarkApplication.class, args);
